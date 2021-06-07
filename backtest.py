0a3e39f1f441207638d27421449408cfd48525bf
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2021-06-08 00:43:18 +0900

    add backtest.py

---
+++ backtest.py
@@ -0,0 +1,12 @@
+class Order():
+
+    def __init__(self, type_, price, volume, stoploss=None, takeprofit=None):
+        self.type = type_
+        self.price price
+        self.volume = volume
+        self.takeprofit = takeprofit
+        self.stoploss = stoploss
+
+    def close(self, price):
+        
+        return profit
