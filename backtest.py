b26ffb5f9aed17c9971b05ff9a3c27f711453a92
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2021-06-08 11:27:37 +0900

    add Order.close()

--- backtest.py
+++ backtest.py
@@ -2,11 +2,22 @@ class Order():
 
     def __init__(self, type_, price, volume, stoploss=None, takeprofit=None):
         self.type = type_
-        self.price price
+        self.price_open = price
+        self.price_close = None
         self.volume = volume
         self.takeprofit = takeprofit
         self.stoploss = stoploss
 
     def close(self, price):
-        
+        self.price_close = price
+        profit = self.price_close - self.price_open
         return profit
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    for i in df:
+        main()
